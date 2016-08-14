0a7470ece28b7614938cec1a13d8813baf5a48be
Md. Mainul Hasan Patwary <mhasan_mitul@yahoo.com>
2016-08-14 20:40:58 +0200

    BigInteger constant added.

--- src/main/java/com/mhneo/PrimeUtil.java
+++ src/main/java/com/mhneo/PrimeUtil.java
@@ -1,7 +1,12 @@
 package com.mhneo;
 
 public class PrimeUtil{
-
+    private BigInteger one = BigInteger.valueOf((long)1); 
+    private BigInteger two = BigInteger.valueOf((long)2);
+    private BigInteger three = BigInteger.valueOf((long)3);
+    private BigInteger five = BigInteger.valueOf((long)5);
+    private BigInteger six = BigInteger.valueOf((long)6);
+    
       public boolean isPrimeForBigInteger(BigInteger num){
         if(num.compareTo(one) == 0 || num.compareTo(one) < 0){
             return false;
